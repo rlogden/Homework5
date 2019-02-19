a3aad0801d212c55126cb7a2814b59c08514c02d
Rachel Ogden <rlogden@dmacc.edu>
2019-02-18 20:09:57 -0600

    Commit to include all Math object stuff

---
+++ src/edu/dmacc/codedsm/hw5/Calculator.java
@@ -0,0 +1,14 @@
+package edu.dmacc.codedsm.hw5;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("The square root of 27 is " + Math.sqrt(27));
+        System.out.println("28 divided by 5 and rounded down is " + Math.floorDiv(28,5));
+        System.out.println("2.2 to the fourth power is " + Math.pow(2.2, 4));
+        System.out.println("Random Integer: " + Math.round(Math.random() * 100));
+
+    }
+
+}
